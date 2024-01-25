fb7165d73be1dcf3910cc602cb3e336b1b2920f7
Senakara <106998274+sena-kara@users.noreply.github.com>
2024-01-26 01:11:40 +0300

    Add files via upload
    Coding traffic control signal commands with Python conditional statements

---
+++ trafikIşıkları.py
@@ -0,0 +1,23 @@
+lights = ["red", "yellow", "green"]
+currentLight = lights[0]
+print(currentLight)
+
+if currentLight == "red":
+    print("STOP!")
+    
+if currentLight == "yellow":
+    print("READY!")
+    
+if currentLight == "green":
+     print("GO!")
+
+currentLight = lights[2]
+print(currentLight)
+if currentLight == "red":
+    print("STOP!")
+    
+if currentLight == "yellow":
+    print("READY!")
+    
+if currentLight == "green":
+     print("GO!")
